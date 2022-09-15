7e1c922fea350fc139bfac018428891be9fdf2de
rakeshrgit <rakesh2abhishek@gmail.com>
2022-09-15 20:57:54 +0530

    changes added

--- src/component/headerbanner/headerBanner.jsx
+++ src/component/headerbanner/headerBanner.jsx
@@ -8,6 +8,7 @@ import "slick-carousel/slick/slick-theme.css";
 import ProjectsContext from '../../context/projectsContext';
 import ReviewSlider from './../../pagesection/reviewSlider';
 import ContactUs from './../../formcontact/contactus';
+import Loader from "../../images/loader.gif";
 class HeaderBanner extends Component {
     state = { 
         pages:[]
@@ -22,42 +23,51 @@ class HeaderBanner extends Component {
        this.context.getAllPageData();
       }
     render() { 
-        const {pages} = this.context;  
-        return ( 
-            <React.Fragment>
-                 {pages.map(item =>
-                    <div key={item.id} className="section-home">
-                        <div className="banner-main d-flex align-items-center flex-column">
-                            <div className="container">
+        const {pages, loading} = this.context;  
+        //console.log('pages', pages)
+        if(pages.length > 0){
+            return ( 
+                <React.Fragment>
+                    {pages.map(item =>
+                        <div key={item.id} className="section-home">
+                            <div className="banner-main d-flex align-items-center flex-column">
                                 <div className="container">
-                                    <div className="content-banner">
-                                        <h2>{item.acf.banner_text}</h2>    
-                                        <div className="contact"><Link to="/">{item.acf.banner_link}</Link></div>  
+                                    <div className="container">
+                                        <div className="content-banner">
+                                            <h2>{item.acf.banner_text}</h2>    
+                                            <div className="contact"><Link to="/">{item.acf.banner_link}</Link></div>  
+                                        </div>
+                                    </div>
+                                    <div className="banner-head">
+                                        <img src={item.acf.banner_image} alt="Merchant"/>
                                     </div>
                                 </div>
-                                <div className="banner-head">
-                                    <img src={item.acf.banner_image} alt="Merchant"/>
+                            </div>
+                            <div className="bg-help">
+                                <SectionAbout  contentabout={item.acf.section_about} />
+                            </div>
+                            <div className="s-benefit">
+                                <AdvantageBlock contentadv={item.acf.section_advantage}/>
+                            </div>
+                            <div className="section-client">
+                                <div className="container">
+                                    <ReviewSlider review={item.acf.review_slider}/>
                                 </div>
                             </div>
-                        </div>
-                        <div className="bg-help">
-                            <SectionAbout  contentabout={item.acf.section_about} />
-                        </div>
-                        <div className="s-benefit">
-                            <AdvantageBlock contentadv={item.acf.section_advantage}/>
-                        </div>
-                        <div className="section-client">
-                            <div className="container">
-                                <ReviewSlider review={item.acf.review_slider}/>
-                            </div>
-                        </div>
-                    </div>    
-                )}           
-                <div>
-                    <ContactUs/>    
-                </div> 
-            </React.Fragment>
-         );
+                        </div>    
+                    )}           
+                    <div>
+                        <ContactUs/>    
+                    </div> 
+                </React.Fragment>
+             );
+        }
+        else{
+            return (<div>
+                <img className="loader" src={Loader} alt="Loader"/>
+            </div>)
+        }
+        
     }
 }
  
